d8ec42389e4c0e5b8ae084f405e685ddad63fa0b
MatGod135 <matheuslealcosta@icloud.com>
2022-08-22 17:25:29 -0300

    Cria tela main

---
+++ src/componets/Main/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './main.css'
+
+
+function Main(){
+    return(
+        <body>
+            <div className="external-div">
+                <div className="main-div">
+                    <p></p>
+                </div>
+            </div>
+        </body>
+    );
+}
+
+export default Main;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Main from './componets/Main';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>
 );
 
