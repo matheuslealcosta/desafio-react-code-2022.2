c8f87b554c356ea73fe366a7c5703311c1cc716e
MatGod135 <matheuslealcosta@icloud.com>
2022-09-01 11:17:10 -0300

    Consome json da api

--- src/componets/Cards/index.js
+++ src/componets/Cards/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import './cards.css'
 
-function Cards({name, email}){
+function Cards({name, email, departamentos, aniversario, cargo}){
     return(
-        <div>
-            <h1>{name}</h1>
-            <p>{email}</p>
+        <div className="cardes">
+            <h1>Nome: {name}</h1>
+            <p>E-mail: {email}</p>
+            <p>Aniversario: {aniversario}</p>
+            <div>
+            <p className="flex-dep">Departamentos: {departamentos.map((dep)=> <div className="div-dep"> { dep}</div>)}</p>
+            </div>
+            <p>Cargo: {cargo}</p>
         </div>
     );
 }
--- src/componets/Membros/index.js
+++ src/componets/Membros/index.js
@@ -1,20 +1,36 @@
-import React from "react";
-import './membros.css'
-import { members } from '../../api.json'
-import Cards from '../Cards'
-
+import React, { useState, useEffect } from "react";
+import "./membros.css";
+import Cards from "../Cards";
 
 function Membros() {
-    return(
-            <ul>
+  const [members, setMembers] = useState([]);
 
-                {members.map((members)=>{
-                    return(
-                        <Cards email={members.email}></Cards>
-                    );
-                })}
-            </ul>
+  useEffect(() => {
+    fetch("http://localhost:3000/members").then((res) =>
+      res.json().then((data) => setMembers(data))
     );
+}, []);
+
+  return (
+    <div className="conteiner-cards">
+    <nav className="nav-cards">
+        {members.map(member =><Cards key={member.id}
+        name = {member.name}
+        email = {member.email}
+        departamentos = {member.departamentos}
+        aniversario = {member.aniversario}
+        cargo = {member.cargo}
+        />)}
+    </nav>
+    </div>
+  );
 }
 
 export default Membros;
+
+
+
+
+
+
+  
