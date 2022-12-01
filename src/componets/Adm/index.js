269c5d5db1a6521e03a964b6591907b8ec24f49c
“MatGod135” <matheuslealcosta@icloud.com>
2022-12-01 18:08:12 -0300

    Pega todos os usuarios na area adm com os botões

--- src/componets/Adm/index.js
+++ src/componets/Adm/index.js
@@ -1,21 +1,51 @@
 import axios from "axios";
 import React from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 import './adm.css'
 
-function Adm(){
+function Adm() {
 
-    useEffect(()=>{
-        axios.get("https://localhost:3000/members").then(()=>{
-            console.log("Todos os membros foram consumidos com sucesso");
-        }).catch(()=>{
+    const [members, setMembers] = useState([]);
+
+    useEffect(() => {
+        axios.get(" http://localhost:3000/members").then((response) => {
+            setMembers(response.data)
+        }).catch(() => {
             console.log("O consumo dos membros por meio do metodo GET falhou");
         })
     }, []);
 
-    return(
-        <div>Olá</div>
-    );
+    return (
+
+        <main>
+            {members.map((members, key) => {
+                return (
+                    <div className="botoes">
+                        <h1>{members.id}</h1>
+                        <h2>{members.name}</h2>
+                        <p>{members.departamentos}</p>
+                        <div className="botaoDeletar">
+                            <button>Excluir</button>
+                        </div>
+
+                        <div className="botaoEditar">
+                            <button>Editar</button>
+                        </div>
+
+                        <div className="botaoVisualizar">
+                            <button>Visualizar</button>
+                        </div>
+
+                    </div>
+                )
+            })
+            }
+        </main>
+
+
+
+    )
 }
 
 export default Adm;
