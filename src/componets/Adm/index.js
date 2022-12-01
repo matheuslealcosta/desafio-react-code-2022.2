452fd7c9fd956164bb3ab2ff1c7deab89094c270
“MatGod135” <matheuslealcosta@icloud.com>
2022-12-01 17:43:53 -0300

    consome os membros por meio do axios em GET

--- src/componets/Adm/index.js
+++ src/componets/Adm/index.js
@@ -1,7 +1,18 @@
+import axios from "axios";
 import React from "react";
+import { useEffect } from "react";
 import './adm.css'
 
 function Adm(){
+
+    useEffect(()=>{
+        axios.get("https://localhost:3000/members").then(()=>{
+            console.log("Todos os membros foram consumidos com sucesso");
+        }).catch(()=>{
+            console.log("O consumo dos membros por meio do metodo GET falhou");
+        })
+    }, []);
+
     return(
         <div>Olá</div>
     );
