0fe7400111aa6c0ca71975b14fd6338fa4f576d1
MatGod135 <matheuslealcosta@icloud.com>
2022-12-02 12:55:18 -0300

    implementa bootstrap

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import Main from "./componets/Main";
 import Navbar from "./componets/Navbar";
 import { BrowserRouter } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
